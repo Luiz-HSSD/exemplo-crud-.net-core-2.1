08f7fe23bf3d04325f7d45743b4350de1e0eff59
luiz-hssd <lhenrique_diniz@hotmail.com>
2019-09-28 16:33:43 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Data/WebApplication2Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication2.Models
+{
+    public class WebApplication2Context : DbContext
+    {
+        public WebApplication2Context(DbContextOptions<WebApplication2Context> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<WebApplication2.Models.Fruit> Fruit { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ WebApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        static WebApplication2.Models.WebApplication2Context db;
+        public static void Main(string[] args)
+        {
+            
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
